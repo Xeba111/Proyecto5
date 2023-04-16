b241b887a5013139ddc6f4e46adf3f48f9638d52
nicol <nicole_milena@live.com>
2023-04-16 15:43:46 -0500

    empezando singleton

---
+++ app/src/main/java/com/example/proyecto4_v1_2/GastosSingleton.java
@@ -0,0 +1,12 @@
+package com.example.proyecto4_v1_2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class GastosSingleton {
+    private static GastosSingleton instanceGastosSingleton = GastosSingleton();
+    private Map<String, List<String>> gastosHistory;
+
+
+}
